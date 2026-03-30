ae6f932e509e371db711afcef5803e8f5ab98ab6
Elhacen18 <elhacencheikh18@gmail.com>
2026-03-30 13:47:05 -0400

    solved new 3 problems

--- SQLPracticeProblems57.sql
+++ SQLPracticeProblems57.sql
@@ -32,3 +32,38 @@ only those employees.
 */
 
 -- Answer
+
+SELECT FirstName, LastName, HireDate FROM Employees WHERE Title='Sales Representative';
+
+/*
+4. Sales Representatives in the United
+States
+Now we’d like to see the same columns as above, but
+only for those employees that both have the title of
+Sales Representative, and also are in the United
+States.
+*/
+-- SELECT * FROM Employees;
+SELECT FirstName, LastName, HireDate FROM Employees WHERE Title='Sales Representative' AND Country='USA';
+
+/*
+5. Orders placed by specific EmployeeID
+Show all the orders placed by a specific employee.
+The EmployeeID for this Employee (Steven
+Buchanan) is 5.
+*/
+-- Note: Find Employee who has EmployeeID of 5.
+SELECT * FROM Employees WHERE EmployeeID=5;
+-- Note: Count the number of orders that have Employee EmployeeID of 5.
+SELECT OrderID, OrderDate FROM Orders WHERE EmployeeID=5;
+
+
+/*
+6. Suppliers and ContactTitles
+In the Suppliers table, show the SupplierID,
+ContactName, and ContactTitle for those Suppliers
+whose ContactTitle is not Marketing Manager.
+*/
+
+SELECT SupplierId, ContactName, ContactTitle FROM Suppliers WHERE  NOT ContactTitle = 'Marketing Manger';
+
